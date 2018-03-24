1dfbe4baaf2ed05f218142e1f977187006a62caf
cheesqwak <cheesqwak@protonmail.com>
2018-03-24 17:37:01 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lie2me
 A small script to check if a python project is backdored
+
+The script is just going to look after system calls, you will have then to check manually if the results are suspicious or not.
--- lie2me.sh
+++ lie2me.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+nbArg=$# 
+
+if test $nbArg -lt 1
+then
+     echo "USAGE   : lie2me.sh <PROJECT DIR OR PROJECT FILE>"
+     echo "EXAMPLE : lie2me.sh /home/cheesqwak/project/"
+     echo "EXAMPLE : lie2me.sh /home/cheesqwak/project/main.py"
+     exit 1
+fi
 
 ioc1="system("
 ioc2="executable"
@@ -6,12 +15,14 @@ ioc3="call("
 ioc4="shell=True"
 ioc5="popen("
 
+#grep in code
 grep -nHr $ioc1 $1
 grep -nHr $ioc2 $1
 grep -nHr $ioc3 $1
 grep -nHr $ioc4 $1
 grep -nHir $ioc5 $1
 
+#grep in compiled code
 find $1 -name "*.pyc" -exec strings {} \; |grep $ioc1
 find $1 -name "*.pyc" -exec strings {} \; |grep $ioc2
 find $1 -name "*.pyc" -exec strings {} \; |grep $ioc3
